53e464792ce7f4b1c7a8cfb67a5f7274e6759821
NamNT <thanhnamk96@gmail.com>
2018-11-10 10:43:28 +0700

    add style

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
         <TouchableOpacity>
-          <Text>Login</Text>
+          <Text style={{color:"red"}}>Login</Text>
           </TouchableOpacity>
       </View>
     );
